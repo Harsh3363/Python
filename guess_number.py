4c6facd91c933a82fae0d5a2eb7a4607e839a180
Harsh Kanodiya <72756692+Harsh3363@users.noreply.github.com>
2022-09-18 16:40:26 +0530

    Create guess_number.py

---
+++ guess_number.py
@@ -0,0 +1,48 @@
+import random
+import clear
+
+def play_again(run_again):
+  if run_again=="y":
+    clear()
+    guess_game()
+
+run_again = input("do you want to play game y or n: ")
+play_again(run_again)
+
+
+def guess_game():
+  print("Welcome to the number guessing game! \nI'm       thinking of a number in between 1 to 100")
+  level= input("choose a difficulty level either easy     or hard: ")
+  
+  if level=="easy":
+    num_attempt = 10
+  else:
+    num_attempt = 5
+    
+  def guess():
+    return random.randint(1,100)
+     
+  continue_game = True
+  ans = guess()
+  
+  while continue_game  : 
+    print(f"you have {num_attempt} number of attempts") 
+    num = int(input("guess a number: "))
+    print(num_attempt)
+    if num>ans: 
+      print("guessed number is too high")
+      num_attempt-=1
+    elif num<ans:
+      print("guessed number is too low")
+      num_attempt-=1
+    elif num_attempt==0:
+      print("you lost")
+      run_again = input("wanna play again?\n y or n: ")
+      play_again(run_again)
+    elif num==ans:
+      print("You guessed it correct\n Congrats won!!") 
+      continue_game = False
+      run_again = input("wanna play again?\ny or n: ")
+      play_again(run_again)
+    
+# guess_game()
