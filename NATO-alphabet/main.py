e8fb0517e66e1f226d66adcbbe3e70e21cd339db
Harsh Kanodiya <72756692+Harsh3363@users.noreply.github.com>
2022-11-16 12:34:58 +0530

    Update main.py

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -5,11 +5,21 @@ import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 new_dic = {word.letter: word.code for (letter, word) in data.iterrows()}
+
 # print(new_dic)
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_name = input("enter any text: ").upper()
-new_list = [new_dic[letter] for letter in user_name]
-# since we can iterate over any sequence using comprehension here we are iterating over a String
-# now for each letter in the string sequence we are passing over the new_dic[letter]
-print(new_list)
+def nato():
+    user_name = input("enter any text: ").upper()
+
+    try:
+        new_list = [new_dic[letter] for letter in user_name]
+    except KeyError:
+        print("Sorry only characters allowed")
+        nato()
+    else:
+        # since we can iterate over any sequence using comprehension here we are iterating over a String
+        # now for each letter in the string sequence we are passing over the new_dic[letter]
+        print(new_list)
+
+nato()
